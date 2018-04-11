0fe6e7e9bdcba150af18bcd8695758169202472e
xianzhen.li <xianzhen.li@husor.com>
2018-04-11 12:01:23 +0800

    first commit

---
+++ app/src/main/java/com/lxz/baselib/Test.java
@@ -0,0 +1,10 @@
+package com.lxz.baselib;
+
+/**
+ * Description:
+ * Created by xianzhen.li on 2018/4/10.
+ */
+
+public class Test {
+
+}
---
+++ basesdk/src/main/java/com/lxz/basesdk/ToastHelper.java
@@ -0,0 +1,57 @@
+package com.lxz.basesdk;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.view.Gravity;
+import android.widget.Toast;
+
+/**
+ * Description:
+ * Created by xianzhen.li on 2018/1/8.
+ */
+public class ToastHelper {
+
+    private static Toast mToast;
+
+    public static void showToast(Context context, CharSequence text) {
+        if (TextUtils.isEmpty(text)) return;
+
+        try {
+            if (mToast == null) {
+                mToast = Toast.makeText(context, text, Toast.LENGTH_LONG);
+                mToast.setGravity(Gravity.CENTER, 0, 0);
+            } else {
+                mToast.setDuration(Toast.LENGTH_LONG);
+                mToast.setText(text);
+            }
+            mToast.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void showToastShort(Context context, CharSequence text) {
+        if (TextUtils.isEmpty(text)) return;
+
+        try {
+            if (mToast == null) {
+                mToast = Toast.makeText(context, text, Toast.LENGTH_SHORT);
+                mToast.setGravity(Gravity.CENTER, 0, 0);
+            } else {
+                mToast.setDuration(Toast.LENGTH_SHORT);
+                mToast.setText(text);
+            }
+            mToast.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void closeToast() {
+        if (mToast != null) {
+            mToast.cancel();
+            mToast = null;
+        }
+    }
+}
